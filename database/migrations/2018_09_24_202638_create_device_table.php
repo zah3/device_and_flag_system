b85e75c0adf47277d244add03baa838cc6845918
Zachariasz <zah3@o2.pl>
2018-09-24 22:53:04 +0200

    [migrations] remove basic migrations and add device model

---
+++ database/migrations/2018_09_24_202638_create_device_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDeviceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('device', function (Blueprint $table) {
+            $table->collation = 'utf8_general_ci';
+            $table->string('serial_number','255')->unique()->primary();
+            $table->timestamp('created_at')->useCurrent();
+            $table->timestamp('updated_at')->default(\Illuminate\Support\Facades\DB::raw('NULL on update CURRENT_TIMESTAMP'))->nullable();
+            $table->text('flag_list');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('device');
+    }
+}
